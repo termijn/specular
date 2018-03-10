c35632b82ab45703b986c33be91cad1c01a1185f
termijn <martijn@vangeloof.com>
2018-03-10 13:09:34 +0100

    Introduced client-side dependencies with NPM

--- server/app.js
+++ server/app.js
@@ -1,11 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => 
-{
-    res.send('Hello World!')
-});
-app.use(express.static('../public'))
+app.use(express.static('public'))
 
 const port = 8095;
 app.listen(port, () => console.log('Speculum Smart Mirror server listening on port ' + port))
