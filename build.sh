a4a316142116c2fa76f818cf33676ef8b09e1481
termijn <martijn@vangeloof.com>
2018-03-17 11:24:03 +0100

    Generate index.html

--- build.sh
+++
@@ -1,2 +0,0 @@
-cp ./public/index.html ./dist/index.html -f
-./node_modules/.bin/webpack --config webpack.config.js
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './public/js/app.js',
-  mode: 'development',
+  mode: 'development', // Set to production in final build
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  plugins: [new HtmlWebpackPlugin({template: './public/index.html'})],
   module: {
     rules: [
       {
